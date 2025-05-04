e54cc52f1f87bf2d6add1b8d3194cd66ad836791
Marcos Ferreira <marcos_ferreira@outlook.pt>
2025-05-04 21:08:26 +0100

    APP_DEBUG

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ if (item) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("id", "tw-ally-help");
     item.appendChild(newDiv)
+    createApp(App).mount('#tw-ally-help')
 }
 
-createApp(App).mount('#tw-ally-help')
+if (import.meta.env.VITE_APP_DEBUG) {
+    createApp(App).mount('#tw-ally-help')
+}
