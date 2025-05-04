916f5527410303f3aa5bcbf88a5717100a609d35
Marcos Ferreira <marcos_ferreira@outlook.pt>
2025-05-04 21:21:34 +0100

    more stuff

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-console.log("Testing")
+import { createApp } from 'vue'
+import App from './App.vue'
+
 alert("asd")
-// import App from './App.vue'
-// import { createApp } from 'vue'
-
-// let item = document.getElementById("ally_content")
-// if (item) {
-//     let newDiv = document.createElement("div");
-//     newDiv.setAttribute("id", "tw-ally-help");
-//     item.appendChild(newDiv)
-//     createApp(App).mount('#tw-ally-help')
-// }
-
+createApp(App).mount('#tw-ally-help')
