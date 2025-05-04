c238d299f7d3b17261f8afa97051dab3bcbcde65
Marcos Ferreira <marcos_ferreira@outlook.pt>
2025-05-04 21:12:08 +0100

    more

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 let item = document.getElementById("ally_content")
-console.log("Testing")
 if (item) {
-    debugger
+    console.log("Testing")
     let newDiv = document.createElement("div");
     newDiv.setAttribute("id", "tw-ally-help");
     item.appendChild(newDiv)
     createApp(App).mount('#tw-ally-help')
 }
 
-if (import.meta.env.VITE_APP_DEBUG) {
-    createApp(App).mount('#tw-ally-help')
-}
